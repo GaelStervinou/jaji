4fa6496954e5f4c90bb47c04c45d605c8ecd70c6
ThibautSembeni <thibaut.sbn.dev@gmail.com>
2024-06-26 14:43:30 +0200

    edit type of content

--- src/Entity/Messages.php
+++ src/Entity/Messages.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MessagesRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation\Timestampable;
 
@@ -20,7 +21,7 @@ class Messages
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $content = null;
 
     #[ORM\ManyToOne(inversedBy: 'messages')]
